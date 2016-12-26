24dfcc1103d34fca773b406f3b3196fc2abc38e4
Björn Dahlgren <bjorn@dahlgren.at>
2016-12-26 22:29:52 +0100

    Basic tests for all API functionality

---
+++ test/templates.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+
+const app = require('../templates');
+
+describe('templates', function() {
+  describe('GET /', function() {
+    it('should respond with json', function(done) {
+      request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+});
